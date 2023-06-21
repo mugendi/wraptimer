565f6602bf56c6af390f66deda1c1bdd7f6436d4
Anthony Mugendi <ngurumugz@gmail.com>
2023-06-21 10:45:23 +0300

    added class doc strings

--- wraptimer/timeit.py
+++ wraptimer/timeit.py
@@ -12,6 +12,94 @@ from .utils import DebugContext, log
 
 
 class TimeIt:
+
+    """# Timeit Class
+    This class exposes two decorators `@byline` and `@func`
+    which work perfectly with both synchronous and asynchronous
+    functions.
+
+    ## Args:
+    - `verbose` bool : Defaults to `True`.
+    If True, then decorators will print execution time
+    to console. If false then the execution time data is added to
+    the function response so that it return a tuple.
+
+        **Example:**
+        ```python
+        # init
+        import pprint
+        timeit_non_verbose = TimeIt(verbose=False)
+
+        # decorate
+        @timeit_non_verbose.byline
+        def test_non_verbose(v=1):
+            time.sleep(1.5)
+            x = v * 50
+            return x
+
+        resp = test_non_verbose(22)
+        pprint.pprint(resp)
+        ```
+        This will output:
+        ```txt
+        (1100,
+            [{'LINE': 75, 'SYNC FUNC': 'test_non_verbose', 'TOOK': '1.501589155 s'},
+            {'LINE': 76, 'SYNC FUNC': 'test_non_verbose', 'TOOK': '29.476 μs'}],
+            [' TOOK: 1.501719173 s'])
+        ```
+        Where:
+
+        1. The first value **1100** is the si the `test_non_verbose()`
+        return value i.e **22*50**
+        2. The second tuple value is a list of all execution times
+        in this case *by line* execution times and *total execution time*
+
+        !!! note
+            Setting `verbose` to `False` is useful in cases where you want
+            to measure execution time and instead of printing, use the values
+            in other ways, such as saving the data to a timeseries database
+            for future reporting on code execution.
+
+    - `show_args` bool : Defaults to `False`. When set to `True`, arguments
+    passed to the decorated function are printed.
+
+        **Example:**
+        ```python
+        # init
+        timeit_non_verbose = TimeIt(show_args=True)
+
+        # decorate
+        @timeit_non_verbose.byline
+        def test_non_verbose(v=1):
+            time.sleep(1.5)
+            x = v * 50
+            return x
+
+        test_non_verbose(22)
+
+        ```
+        Logs:
+        ![](images/)
+        ```txt
+        ━━━━━━━━━━━━━━━━━━━━━━━ [ SYNC FUNC: test_non_verbose ] ━━━━━━━━━━━━━━━━━━━━━━━
+        LINE: 89, TOOK: 1.500271156 s
+        LINE: 90, TOOK: 320.156 μs
+        ------------------------------------------------------------------------------
+        ARGS: (22,)
+        KWARGS: {}
+        TOOK: 1.50086468 s
+        ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+        ```
+        !!! note
+            Values of `ARGS: (22,)` and `KWARGS: {}` are printed.
+
+            This setting is important foe when you are measuring execution
+            times of the same functions but passing varying arguments.
+            That way you c`an see how each argument affects execution time.
+
+
+    """
+
     def __init__(self, verbose: bool = True, show_args: bool = False):
         self.verbose = verbose
         self.show_args = show_args
